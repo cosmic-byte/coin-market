ff24a228481e3f831718d1d0d18a1e336f80497c
gregobinna <gregobinna@gmail.com>
2018-01-03 17:38:35 +0100

    add base class for test and test for user model

---
+++ app/test/test_user_model.py
@@ -0,0 +1,25 @@
+import unittest
+
+import datetime
+
+from app.portal import db
+from app.portal.models.user import User
+from app.test.base import BaseTestCase
+
+
+class TestUserModel(BaseTestCase):
+
+    def test_encode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test',
+            registered_on=datetime.datetime.utcnow()
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+
+if __name__ == '__main__':
+    unittest.main()
+
