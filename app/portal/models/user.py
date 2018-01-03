01e7bd54af7584477f9d7b110a5980ffdee67db1
gregobinna <gregobinna@gmail.com>
2018-01-03 18:00:19 +0100

    add method for decoding auth token and test

--- app/portal/models/user.py
+++ app/portal/models/user.py
@@ -50,5 +50,20 @@ class User(db.Model):
         except Exception as e:
             return e
 
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, key)
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
     def __repr__(self):
         return "<User '{}'>".format(self.username)
--- app/test/test_user_model.py
+++ app/test/test_user_model.py
@@ -20,6 +20,19 @@ class TestUserModel(BaseTestCase):
         auth_token = user.encode_auth_token(user.id)
         self.assertTrue(isinstance(auth_token, bytes))
 
+    def test_decode_auth_token(self):
+        user = User(
+            email='test@test.com',
+            password='test',
+            registered_on=datetime.datetime.utcnow()
+        )
+        db.session.add(user)
+        db.session.commit()
+        auth_token = user.encode_auth_token(user.id)
+        self.assertTrue(isinstance(auth_token, bytes))
+        self.assertTrue(User.decode_auth_token(auth_token) == 1)
+
+
 if __name__ == '__main__':
     unittest.main()
 
