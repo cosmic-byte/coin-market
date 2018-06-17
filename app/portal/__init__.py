4e1c0d74146d46db0857e83bfd3f21cc950fab94
gregobinna <gregobinna@gmail.com>
2018-06-17 22:49:08 +0100

    refactor templates

--- app/portal/__init__.py
+++ app/portal/__init__.py
@@ -3,15 +3,12 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_bootstrap import Bootstrap
 
 from .config import config_by_name
-from .nav import nav
 
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
 socketio = SocketIO()
-bootstrap = Bootstrap()
 cors = CORS()
 
 
@@ -21,9 +18,7 @@ def create_app(config_name):
     db.init_app(app)
     flask_bcrypt.init_app(app)
     cors.init_app(app)
-    bootstrap.init_app(app)
     socketio.init_app(app)
-    nav.init_app(app)
 
     return app
 
