4a9fdfe3c5616fe3b7824df8c6a8195715b24f06
gregobinna <gregobinna@gmail.com>
2018-06-18 08:02:49 +0100

    clean up

--- app/portal/frontend.py
+++ app/portal/frontend.py
@@ -1,10 +1,3 @@
-# This contains our frontend; since it is a bit messy to use the @app.route
-# decorator style when using application factories, all of our routes are
-# inside blueprints. This is the front-facing blueprint.
-#
-# You can find out more about blueprints at
-# http://flask.pocoo.org/docs/blueprints/
-
 from flask import Blueprint, render_template, flash, redirect, url_for
 from markupsafe import escape
 
@@ -18,8 +11,8 @@ def index():
 
 
 # Shows a long signup form, demonstrating form rendering.
-@frontend.route('/form/', methods=('GET', 'POST'))
-def form():
+@frontend.route('/sign-up/', methods=('GET', 'POST'))
+def sign_up():
     form = SignupForm()
 
     if form.validate_on_submit():
@@ -34,4 +27,4 @@ def form():
         # In a real application, you may wish to avoid this tedious redirect.
         return redirect(url_for('.index'))
 
-    return render_template('404.html', form=form)
+    return render_template('index.html', form=form)
